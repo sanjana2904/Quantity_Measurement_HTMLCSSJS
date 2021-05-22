29f72e230387bc98e7bd18c054e4fb026edd83f0
sanjana k <sanjanarthi@gmail.com>
2021-05-22 09:23:08 +0530

    Added html,css,JS

---
+++ Javascript/Home.js
@@ -0,0 +1,9 @@
+var input = document.getElementById('input');
+var output = document.getElementById('output');
+var inputType = document.getElementById('select1');
+var outputValue = document.getElementById('select2');
+var inputTypeValue, outputTypeValue
+$(document).ready(function () {
+	
+});
+    
